2959b17391e0fd5e90657b93d2dfe31c5ae0ff64
Rajarshi Banerjee <rajarshiban13@gmail.com>
2021-08-18 21:26:02 +0530

    dsp3

---
+++ assignment3.m
@@ -0,0 +1,93 @@
+x=linspace(-10,10,21);
+d1=logical(dirac(x));
+d2=logical(dirac(x-1));
+d3=logical(dirac(x-2));
+f1=(fft(d1));
+f2=(fft(d2));
+f3=(fft(d3));
+figure(1);
+subplot(2,2,1);
+plot(x,real(f1));
+grid on;
+xlabel('x (discrete values)');
+ylabel('real value of fft{dirac delta at x=0}');
+title('real part vs discrete sample values');
+
+subplot(2,2,2);
+plot(x,imag(f1));
+grid on;
+xlabel('x (discrete values)');
+ylabel('imaginary value of fft{dirac delta at x=0}');
+title('imaginary part vs discrete sample values');
+
+subplot(2,2,3);
+plot(x,abs(f1));
+grid on;
+xlabel('x (discrete values)');
+ylabel('imaginary value of fft{dirac delta at x=0}');
+title('imaginary part vs discrete sample values');
+
+subplot(2,2,4);
+plot(x,atand(imag(f1)./real(f1)));
+grid on;
+xlabel('x (discrete values)');
+ylabel('imaginary value of fft{dirac delta at x=0}');
+title('imaginary part vs discrete sample values');
+
+figure(2)
+subplot(2,2,1);
+plot(x,real(f2));
+grid on;
+xlabel('x (discrete values)');
+ylabel('real value of fft{dirac delta at x=1}');
+title('real part vs discrete sample values');
+
+subplot(2,2,2);
+plot(x,imag(f2));
+grid on;
+xlabel('x (discrete values)');
+ylabel('imaginary value of fft{dirac delta at x=1}');
+title('imaginary part vs discrete sample values');
+
+subplot(2,2,3);
+plot(x,abs(f2));
+grid on;
+xlabel('x (discrete values)');
+ylabel('imaginary value of fft{dirac delta at x=1}');
+title('imaginary part vs discrete sample values');
+
+subplot(2,2,4);
+plot(x,atand(imag(f2)./real(f2)));
+grid on;
+xlabel('x (discrete values)');
+ylabel('imaginary value of fft{dirac delta at x=1}');
+title('imaginary part vs discrete sample values');
+
+figure(3)
+subplot(2,2,1);
+plot(x,real(f3));
+grid on;
+xlabel('x (discrete values)');
+ylabel('real value of fft{dirac delta at x=2}');
+title('real part vs discrete sample values');
+
+subplot(2,2,2);
+plot(x,imag(f3));
+grid on;
+xlabel('x (discrete values)');
+ylabel('imaginary value of fft{dirac delta at x=2}');
+title('imaginary part vs discrete sample values');
+
+subplot(2,2,3);
+plot(x,abs(f3));
+grid on;
+xlabel('x (discrete values)');
+ylabel('imaginary value of fft{dirac delta at x=2}');
+title('imaginary part vs discrete sample values');
+
+subplot(2,2,4);
+plot(x,atand(imag(f3)./real(f3)));
+grid on;
+xlabel('x (discrete values)');
+ylabel('imaginary value of fft{dirac delta at x=2}');
+title('imaginary part vs discrete sample values');
